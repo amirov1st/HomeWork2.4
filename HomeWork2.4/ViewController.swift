a2a18c4cb5b596532661a90a400001db1ae24a86
amir <amirov.foma.icloud.com>
2024-03-14 10:03:44 +0300

    Initial Commit

---
+++ HomeWork2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2.4
+//
+//  Created by Amirov Foma on 14.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
